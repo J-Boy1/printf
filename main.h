27b3aeed3399191298b0d0b0b99982eb0080d0d4
Uche <101931543+J-Boy1@users.noreply.github.com>
2022-10-19 11:30:53 +0100

    main .h

--- main.h
+++ main.h
@@ -1,13 +1,38 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list, flags_t *f);
+} print_t;
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
 int _putchar(char c);
-int print_string(char *);
-int print_decimal(int value);
-long long int power(int base, int x);
-long long convertToDecimal(int Number, int base);
-int print_binary(int number);
-int print_unsigned(unsigned int number);
-
-#endif
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+int get_flag(char s ,flags_t *f);
+#endif  /* _MAIN_H_ */
